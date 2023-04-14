76f78013a6a311c00d5b3935b4423e8bc7cb5458
mikevassalla <46818410+mikevassalla@users.noreply.github.com>
2023-04-13 22:38:47 -0500

    Add files via upload

--- project3Client/src/main/java/GuiClient.java
+++ project3Client/src/main/java/GuiClient.java
@@ -37,9 +37,10 @@ public class GuiClient extends Application{
 	MenuItem exitBtn = new MenuItem("Exit");
 	MenuItem themeBtn = new MenuItem("Change Theme");
 	ArrayList<ImageView> p1Cards = new ArrayList<>(initializeCards());
+	ArrayList<ImageView> p2Cards = new ArrayList<>(initializeCards());
 	int playWin = 0;
 	int opWin = 0;
-	private String player;
+	private Integer player;
 	private int anteWager = 5;
 	private int pairWager = 5;
 	String pfSwitch;
@@ -52,8 +53,11 @@ public class GuiClient extends Application{
 	Button dealBtn = new Button("Deal");
 	Button pairDown = new Button("-");
 	Button pairUp = new Button("+");
-	private Integer changeTheme;
-	
+	TextField anteAmount = new TextField();
+	private Integer changeTheme = 0;
+	TextField yourWinnings = new TextField(Integer.toString(playWin));
+	HBox p1CardsIMG = new HBox(p1Cards.get(0), p1Cards.get(1), p1Cards.get(2));
+	Image temp = new Image("back.png");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -61,19 +65,6 @@ public class GuiClient extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		/*
-		Platform.runLater(()->{
-			listItems.getItems().add(data.toString());
-		});
-		*/
-		
-		//listItems = new ListView<String>();
-		//listItems2 = new ListView<String>();
-		
-		//c1 = new TextField();
-		//b1 = new Button("Send");
-		//b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
 		
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
@@ -98,7 +89,6 @@ public class GuiClient extends Application{
 		});
 		
 		
-		
 		tempB1.setOnAction(e->{	
 			try {
 				clientConnection = new Client("127.0.0.1", 5555, x ->{
@@ -113,7 +103,9 @@ public class GuiClient extends Application{
 			        		pairUp.setDisable(false);
 			            }
 			            else if(r.getResponse() == 2) {
-			            	Image temp = new Image(r.cards.get(0).getImageName());
+			            	//Sends Player Cards
+			            	System.out.println("In 2");
+			            	temp = new Image(r.cards.get(0).getImageName());
 			                ImageView card = new ImageView(temp);
 			            	p1Cards.get(0).setImage(card.getImage());
 			            	temp = new Image(r.cards.get(1).getImageName());
@@ -125,13 +117,81 @@ public class GuiClient extends Application{
 			            	playBtn.setDisable(false);
 			            	foldBtn.setDisable(false);
 			            }
+			            else if(r.getResponse() == 3) {
+			            	//Sends Dealer Cards
+			            	Image temp = new Image(r.cards.get(0).getImageName());
+			                ImageView card = new ImageView(temp);
+			            	p2Cards.get(0).setImage(card.getImage());
+			            	temp = new Image(r.cards.get(1).getImageName());
+			                card = new ImageView(temp);
+			            	p2Cards.get(1).setImage(card.getImage());
+			            	temp = new Image(r.cards.get(2).getImageName());
+			                card = new ImageView(temp);
+			            	p2Cards.get(2).setImage(card.getImage());
+			            }
+			            else if(r.getResponse() == 4) {
+			            	//Tie
+			            	playWin += r.getAnte();
+			            	playWin += r.getAnte();
+			    			playWin += r.getPair();
+			    			yourWinnings.setText(Integer.toString(playWin));
+			    			playBtn.setDisable(true);
+			            	foldBtn.setDisable(true);
+			            	anteDown.setDisable(false);
+			        		anteUp.setDisable(false);
+			            	dealBtn.setDisable(false);
+			        		pairDown.setDisable(false);
+			        		pairUp.setDisable(false);
+			            }
+			            else if(r.getResponse() == 5) {
+			            	//Player 1 Wins
+			            	playWin += r.getAnte();
+			    			playWin += r.getPair();
+			    			yourWinnings.setText(Integer.toString(playWin));
+			    			playBtn.setDisable(true);
+			            	foldBtn.setDisable(true);
+			            	anteDown.setDisable(false);
+			        		anteUp.setDisable(false);
+			            	dealBtn.setDisable(false);
+			        		pairDown.setDisable(false);
+			        		pairUp.setDisable(false);
+			            }
+			            else if(r.getResponse() == 6) {
+			            	//Dealer Wins
+			            	playWin += r.getPair();
+			            	yourWinnings.setText(Integer.toString(playWin));
+			            	playBtn.setDisable(true);
+			            	foldBtn.setDisable(true);
+			            	anteDown.setDisable(false);
+			        		anteUp.setDisable(false);
+			            	dealBtn.setDisable(false);
+			        		pairDown.setDisable(false);
+			        		pairUp.setDisable(false);
+			            }
+			            else if(r.getResponse() == 7) {
+			            	//Dealer does not have queen or higher
+			            	playWin += r.getAnte();
+			            	playWin += r.getAnte();
+			            	anteWager = r.getAnte();
+			            	yourWinnings.setText(Integer.toString(playWin));
+			            	anteAmount.setText(Integer.toString(r.getAnte()));
+			            	anteDown.setDisable(true);
+			        		anteUp.setDisable(true);
+			        		playBtn.setDisable(true);
+			            	foldBtn.setDisable(true);
+			            	dealBtn.setDisable(false);
+			        		pairDown.setDisable(false);
+			        		pairUp.setDisable(false);
+			            }
 			            else if(r.getResponse() == 20) {
-			            	player = r.getMessage();
+			            	System.out.println(r.getMessage());
+			            	player = Integer.parseInt(r.getMessage());
 			            }
 			        });		
 				});
 				clientConnection.start();
 				primaryStage.setScene(clientGameScene());
+				primaryStage.setMaximized(true);
 			}
 			catch(Exception m) {
 				System.out.println(m);
@@ -144,22 +204,19 @@ public class GuiClient extends Application{
 	        System.exit(0);
 		});
 
-		primaryStage.setMaximized(true);
 		primaryStage.setScene(createClientGui());
 		primaryStage.setTitle("Client");	
 		primaryStage.show();
-		
 	}
 	
 	public Scene createClientGui() {
-
 		ip.setFocusTraversable(false);
-		port.setMaxWidth(200); // set the maximum width to 200 pixels
+		port.setMaxWidth(100); // set the maximum width to 200 pixels
 		port.setPromptText("Enter port here");
 		port.setAlignment(Pos.CENTER);
 		port.setFocusTraversable(false);
 
-		ip.setMaxWidth(300); // set the maximum width to 200 pixels
+		ip.setMaxWidth(200); // set the maximum width to 200 pixels
 		ip.setPromptText("Enter IP here");
 		ip.setAlignment(Pos.CENTER);
 
@@ -183,29 +240,20 @@ public class GuiClient extends Application{
 				)
 		);
 		clientBox.setBackground(new Background(backgroundCasino));
-		return new Scene(clientBox, 400, 300);
+		return new Scene(clientBox, 500, 400);
 	}
 	
 	public Scene clientGameScene() {
-		anteDown.setDisable(true);
-		anteUp.setDisable(true);
 		playBtn.setDisable(true);
 		foldBtn.setDisable(true);
-		dealBtn.setDisable(true);
-		pairDown.setDisable(true);
-		pairUp.setDisable(true);
 
-		ArrayList<ImageView> dealerCards = new ArrayList<>(initializeCards());
-		//ArrayList<ImageView> p1Cards = new ArrayList<>(initializeCards());
-		ArrayList<ImageView> p2Cards = new ArrayList<>(initializeCards());
 
 		//
 		//Top row
 		//
-		Label dealerLabel = new Label("Dealer");
+		Label dealerLabel = new Label("Text");
 		Label p1Win = new Label("Your Winnings");
 		Label p2Win = new Label("Opponents Winnings");
-		TextField yourWinnings = new TextField(Integer.toString(playWin));
 		yourWinnings.setEditable(false);
 		yourWinnings.setPrefWidth(50); // set the preferred width to 50 pixels
 		TextField opWinnings = new TextField(Integer.toString(opWin));
@@ -215,9 +263,9 @@ public class GuiClient extends Application{
 
 		VBox yourDisplay = new VBox(p1Win, yourWinnings);
 		VBox opDisplay = new VBox(p2Win, opWinnings);
-		HBox dCards = new HBox(dealerCards.get(0), dealerCards.get(1), dealerCards.get(2));
-		dCards.setSpacing(20); // Set spacing between the cards
-		VBox dealerDisplay = new VBox(dealerLabel, dCards);
+		//HBox dCards = new HBox(dealerCards.get(0), dealerCards.get(1), dealerCards.get(2));
+		//dCards.setSpacing(20); // Set spacing between the cards
+		VBox dealerDisplay = new VBox(dealerLabel);
 		dealerDisplay.setSpacing(10); // Set spacing between the dealer label and the cards
 		dealerDisplay.setAlignment(Pos.CENTER);
 		HBox top = new HBox(yourDisplay, dealerDisplay, opDisplay);
@@ -226,9 +274,9 @@ public class GuiClient extends Application{
 		
 		//Mid row
 		Label p1Label = new Label("You");
-		Label p2Label = new Label("Opponent");
+		Label p2Label = new Label("Dealer");
 		
-		HBox p1CardsIMG = new HBox(p1Cards.get(0), p1Cards.get(1), p1Cards.get(2));
+		
 		p1CardsIMG.setSpacing(20); // Set spacing between the cards
 		HBox p2CardsIMG = new HBox(p2Cards.get(0), p2Cards.get(1), p2Cards.get(2));
 		p2CardsIMG.setSpacing(20); // Set spacing between the cards
@@ -250,7 +298,6 @@ public class GuiClient extends Application{
 		//Bottom row
 		//Ante Col
 		Label ante = new Label("Ante");
-		TextField anteAmount = new TextField();
 		anteAmount.setText(String.valueOf(anteWager));
 		anteAmount.setEditable(false);
 		//Button anteDown = new Button("-");
@@ -280,14 +327,19 @@ public class GuiClient extends Application{
 		//totalPotAmount.setStyle("-fx-background-color: transparent;");
 
 		playBtn.setOnAction(e->{
+			playWin -= anteWager;
+			yourWinnings.setText(Integer.toString(playWin));
 			clientConnection.send(new Responses(3, anteWager, pairWager, player));
 		});
 		
 		foldBtn.setOnAction(e->{
-			clientConnection.send(new Responses(1));
+			clientConnection.send(new Responses(4, anteWager, pairWager, player));
 		});
 		
 		dealBtn.setOnAction(e->{
+			playWin -= anteWager;
+			playWin -= pairWager;
+			yourWinnings.setText(Integer.toString(playWin));
 			clientConnection.send(new Responses(2, anteWager, pairWager, player));
 			anteDown.setDisable(true);
     		anteUp.setDisable(true);
@@ -296,10 +348,10 @@ public class GuiClient extends Application{
     		pairUp.setDisable(true);
 		});
 
-		Label playWagerTxt = new Label("Play Wager");
-		TextField playWagerAmt = new TextField();
-		playWagerAmt.setPrefWidth(50);
-		VBox playDisplay = new VBox(playWagerTxt, playWagerAmt,playBtn, foldBtn, dealBtn);
+		//Label playWagerTxt = new Label("Play Wager");
+		//TextField playWagerAmt = new TextField();
+		//playWagerAmt.setPrefWidth(50);
+		VBox playDisplay = new VBox(playBtn, foldBtn, dealBtn);
 
 		playDisplay.setAlignment(Pos.CENTER);
 		playDisplay.setSpacing(10); // Set spacing between the label and the wager buttons.
@@ -350,7 +402,7 @@ public class GuiClient extends Application{
 		//
 		themeBtn.setOnAction((ActionEvent t) -> {
 			if (changeTheme == 0) {
-				root.getStylesheets().add(getClass().getResource("ClientBlue.css").toExternalForm());
+				root.getStylesheets().add(getClass().getResource("white.css").toExternalForm());
 				changeTheme++;
 				System.out.println(changeTheme);
 			}
@@ -360,7 +412,7 @@ public class GuiClient extends Application{
 				System.out.println(changeTheme);
 			}
 			else if(changeTheme == 2) {
-				root.getStylesheets().add(getClass().getResource("ClientBlue.css").toExternalForm());
+				root.getStylesheets().add(getClass().getResource("clientBlue.css").toExternalForm());
 				changeTheme = 0;
 				System.out.println(changeTheme);
 			}
@@ -377,8 +429,9 @@ public class GuiClient extends Application{
 		menu.getMenus().addAll(m);
 		root.setTop(menu);
 		root.setCenter(game);
-		//root.getStylesheets().add(getClass().getResource("ClientBlue.css").toExternalForm());
+		root.getStylesheets().add(getClass().getResource("ClientBlue.css").toExternalForm());
 		Scene scene = new Scene(root, 700, 700);
+
 		return scene;
 
 
